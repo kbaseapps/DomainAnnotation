0be0883aa8214fcb62a7c85c201c074a07f52d62
John-Marc Chandonia <JMChandonia@lbl.gov>
2016-07-20 23:38:18 +0000

    Added shock library test

--- test/src/domainannotation/test/DomainAnnotationServerTest.java
+++ test/src/domainannotation/test/DomainAnnotationServerTest.java
@@ -1,13 +1,11 @@
 package domainannotation.test;
 
-import java.io.File;
+import java.io.*;
+import java.util.*;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-import junit.framework.Assert;
+import org.junit.Test;
+import static junit.framework.Assert.*;
 
 import org.ini4j.Ini;
 import org.junit.AfterClass;
@@ -16,20 +14,16 @@ import org.junit.Test;
 
 import domainannotation.*;
 import us.kbase.auth.AuthToken;
-import us.kbase.common.service.JsonServerSyslog;
-import us.kbase.common.service.RpcContext;
-import us.kbase.common.service.UObject;
-import us.kbase.workspace.CreateWorkspaceParams;
-import us.kbase.workspace.ObjectSaveData;
-import us.kbase.workspace.ProvenanceAction;
-import us.kbase.workspace.SaveObjectsParams;
-import us.kbase.workspace.WorkspaceClient;
-import us.kbase.workspace.WorkspaceIdentity;
+import us.kbase.shock.client.*;
+import us.kbase.common.service.*;
+import us.kbase.workspace.*;
+
 
 public class DomainAnnotationServerTest {
     private static AuthToken token = null;
     private static Map<String, String> config = null;
     private static WorkspaceClient wsClient = null;
+    private static BasicShockClient shockClient = null;
     private static String wsName = null;
     private static DomainAnnotationServer impl = null;
     
@@ -42,6 +36,8 @@ public class DomainAnnotationServerTest {
         config = ini.get("DomainAnnotation");
         wsClient = new WorkspaceClient(new URL(config.get("workspace-url")), token);
         wsClient.setAuthAllowedForHttp(true);
+        BasicShockClient shockClient = new BasicShockClient(new URL(config.get("workspace-url")), token);
+        
         // These lines are necessary because we don't want to start linux syslog bridge service
         JsonServerSyslog.setStaticUseSyslog(false);
         JsonServerSyslog.setStaticMlogFile(new File(config.get("scratch"), "test.log").getAbsolutePath());
@@ -61,6 +57,33 @@ public class DomainAnnotationServerTest {
         return new RpcContext().withProvenance(Arrays.asList(new ProvenanceAction()
             .withService("DomainAnnotation").withMethod("please_never_use_it_in_production")
             .withMethodParams(new ArrayList<UObject>())));
+    }
+
+    /**
+       Check that we can get all the All Libraries DomainModelSet from the
+       public workspace.
+    */
+    @Test
+    public void checkDMS() throws Exception {
+        String domainWsName = "KBasePublicGeneDomains";
+        String allRef = domainWsName+"/All";
+        
+        DomainModelSet dms = wsClient.getObjects(Arrays.asList(new ObjectIdentity().withRef(allRef))).get(0).getData().asClassInstance(DomainModelSet.class);
+
+        Map<String,String> domainLibMap = dms.getDomainLibs();
+
+        for (String id : domainLibMap.values()) {
+            DomainLibrary dl = wsClient.getObjects(Arrays.asList(new ObjectIdentity().withRef(id))).get(0).getData().asClassInstance(DomainLibrary.class);
+            System.out.println("Testing shock files for "+dl.getSource()+" "+dl.getVersion());
+            for (Handle h : dl.getLibraryFiles()) {
+                System.out.println("  testing "+h.getFileName()+", "+h.getShockId());
+                File f = new File("/tmp/"+h.getFileName());
+                OutputStream os = new BufferedOutputStream(new FileOutputStream(f));
+                shockClient.getFile(new ShockNodeId(h.getShockId()),os);
+                os.close();
+                assertTrue(f.length() > 0);
+            }
+        }
     }
     
     @AfterClass
